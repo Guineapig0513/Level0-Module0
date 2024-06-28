bf16c03c74a795eaea9d6899fb6e75e47fef5213
lvl0_module2 <zoey.lestyk@gmail.com>
2024-06-28 11:56:34 -0700

    jun 28 fri

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,12 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String code = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(code.equals("yes")) 
+JOptionPane.showMessageDialog(null,  "you will rule the world also join the league just please");
+else
+	JOptionPane.showMessageDialog(null,  "sign up for classes at the league because you are very bad at coding YOUUUUUUUUU SUCK");
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,10 +1,53 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
+		int score = 0;
 		
+		String spain = JOptionPane.showInputDialog("What is the capital of Spain?");
+		if(spain.equals("Madrid")) {
+		JOptionPane.showMessageDialog(null,  "Correct :D");
+		score += 1;
+		} else {score -= 1;
+			JOptionPane.showMessageDialog(null,  "Wrong >:(");}
+		
+		String cali = JOptionPane.showInputDialog("What is the capital of California?");
+		if(cali.equals("Sacramento")) {
+		JOptionPane.showMessageDialog(null,  "Correct :D");
+		score += 1;
+		} else {score -= 1;
+			JOptionPane.showMessageDialog(null,  "Wrong >:(");}
+		
+		String pig = JOptionPane.showInputDialog("Guinea pig is cute?");
+		if(pig.equals("Yes")) {
+		JOptionPane.showMessageDialog(null,  "Correct :D");
+		score += 1;
+		}else if(pig.equals("ugly")){
+			JOptionPane.showMessageDialog(null,  "You're a Moron -John Martson");
+			score -= 1000000000;
+		}
+		else {score -= 1;
+			JOptionPane.showMessageDialog(null,  "Wrong >:(");}
+	
+		String java = JOptionPane.showInputDialog("True or false? Java was created in 1995?");
+		if(java.equals("True")) {
+		JOptionPane.showMessageDialog(null,  "Correct :D");
+		score += 1;
+		} else {score -= 1;
+			JOptionPane.showMessageDialog(null,  "Wrong >:(");}
+		
+		String russia = JOptionPane.showInputDialog("Which country has the biggest territory");
+		if(russia.equals("Russia")) {
+		JOptionPane.showMessageDialog(null,  "Correct :D");
+		score += 1;
+		} else {score -= 1;
+			JOptionPane.showMessageDialog(null,  "Wrong >:(");}
+		
+			JOptionPane.showMessageDialog(null, "your score is " +score);
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
